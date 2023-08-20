24be2fa49478e16f2a48ca5282c403f0ebda1e03
Bob Goldham <sold@sold.rip>
2023-08-20 22:07:00 +0200

    FAT12 Filesystem Support, Bootloader can now load kernel

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -10,7 +10,7 @@ jmp short start
 nop
 
 bdb_oem:                    db 'WHACK1.0'           ; 8 bytes, should *probably* be 'MSWIN4.1' but idrk
-bdb_bytes:                  dw 512
+bdb_bytes_per_sector:                  dw 512
 bdb_sectors_per_cluster:    db 1
 bdb_reserved_sectors:       dw 1
 bdb_fat_count:              db 2
@@ -28,7 +28,7 @@ ebr_drive_number:           db 0                    ; 0x00 = floppy, 0x80 = hdd;
                             db 0                    ; reserved. Always 0.
 ebr_signature:              db 29h
 ebr_volume_id:              db 78h, 56h, 34h, 12h   ; serial number. irrelevant.
-ebr_volume_label:           db 'WHACK OS   '        ; Must be 11 bytes, padded with spaces
+ebr_volume_label:           db 'WHACK!OS   '        ; Must be 11 bytes, padded with spaces
 ebr_system_id:              db 'FAT12   '           ; 8 bytes
 
 ;
@@ -36,7 +36,192 @@ ebr_system_id:              db 'FAT12   '           ; 8 bytes
 ;
 
 start:
-    jmp main
+    ; setup data segments
+    mov ax, 0           ; because we cannot write to ds/es directly
+    mov ds, ax
+    mov es, ax
+
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
+
+    ; some BIOSes might start execution at 07C0:0000 instead of 0000:7C00.
+    ; This ensures we are where we expect to be.
+    push es
+    push word .after
+    retf
+.after:
+
+    ; read something from floppy disk
+    ; BIOS should set DL to drive number
+    mov [ebr_drive_number], dl
+
+    ; show loading message
+    mov si, msg_loading
+    call puts
+
+    ; read drive parameters, instead of relying on data from formatted disk
+    push es
+    mov ah, 08h
+    int 13h
+    jc floppy_error
+    pop es
+
+    and cl, 0x3F                    ; remove top 2 bits
+    xor ch, ch
+    mov [bdb_sectors_per_track], cx ; sector count
+
+    inc dh
+    mov [bdb_heads], dh             ; head count
+
+    ; read FAT root directory
+    ; compute LBA on root dir = reserved + fats * sectors_per_fat
+    ; note: this section can be hardcoded.
+    mov ax, [bdb_sectors_per_fat]
+    mov bl, [bdb_fat_count]
+    xor bh, bh
+    mul bx                          ; ax = (fats * sectors_per_fat)
+    add ax, [bdb_reserved_sectors]  ; ax = LBA of root directory
+    push ax
+
+    ; compute size of root directory ) (32 * number_of_entries) / bytes_per_sector
+    mov ax, [bdb_dir_entries_count]
+    shl ax, 5                       ; ax *= 32
+    xor dx, dx                      ; dx = 0
+    div word [bdb_bytes_per_sector] ; number of sectors we need to read
+
+    test dx, dx                     ; if dx != 0, add 1
+    jz .root_dir_after
+    inc ax                          ; division remainder != 0, add 1
+                                    ; this means a partially filled sector
+.root_dir_after:
+
+    ; read root directory
+    mov cl, al                      ; cl = number of sectors to read = size of root dir
+    pop ax                          ; ax = LBA of root dir
+    mov dl, [ebr_drive_number]      ; dl = drive number (which we saved earlier)
+    mov bx, buffer                  ; es:bx = buffer
+    call disk_read
+
+    ; search for kernel.bin
+    xor bx, bx
+    mov di, buffer
+
+.search_kernel:
+    mov si, file_kernel_bin
+    mov cx, 11                      ; compare up to 11 characters. Which is a full FAT12 filename
+    push di
+    repe cmpsb
+    pop di
+    je .found_kernel
+
+    add di, 32
+    inc bx
+    cmp bx, [bdb_dir_entries_count]
+    jl .search_kernel
+
+    ; kernel not found
+    jmp kernel_not_found_error
+
+.found_kernel:
+
+    ; di should have the address to the entry
+    mov ax, [di + 26]               ; first logical cluster field (offset by 26)
+    mov [kernel_cluster], ax
+
+    ; load FAT from disk into memory
+    mov ax, [bdb_reserved_sectors]
+    mov bx, buffer
+    mov cl, [bdb_sectors_per_fat]
+    mov dl, [ebr_drive_number]
+    call disk_read
+
+    ; read kernel and process FAT chain
+    mov bx, KERNEL_LOAD_SEGMENT
+    mov es, bx
+    mov bx, KERNEL_LOAD_OFFSET
+
+.load_kernel_loop:
+
+    ; Read next cluster
+    mov ax, [kernel_cluster]
+
+    ; this is bad. We shouldn't hardcode this offset.
+    add ax, 31                      ; first cluster = (cluster number -2) * sectors_per_cluster + start_sector
+                                    ; start sector = reserved + fats + rootDirSize = 1 + 18 + 134 = 33
+    mov cl, 1
+    mov dl, [ebr_drive_number]
+    call disk_read
+
+    ; this will probably overflow at some point... should fix it then maybe?
+    add bx, [bdb_bytes_per_sector]
+
+    ; compute location of next cluster
+    mov ax, [kernel_cluster]
+    mov cx, 3
+    mul cx
+    mov cx, 2
+    div cx                          ; ax = index of entry in FAT, dx = cluster mod2
+
+    mov si, buffer
+    add si, ax
+    mov ax, [ds:si]                 ; read entry from FAT table at index ax
+
+    or dx, dx
+    jz .even
+
+.odd:
+    shr ax, 4
+    jmp .next_cluster_after
+
+.even:
+    and ax, 0x0FFF
+
+.next_cluster_after:
+    cmp ax, 0x0FF8                  ; end of chain
+    jae .read_finish
+
+    mov [kernel_cluster], ax
+    jmp .load_kernel_loop
+
+.read_finish:
+
+    ; far-jump to kernel
+    mov dl, [ebr_drive_number]      ; boot device in dl
+    mov ax, KERNEL_LOAD_SEGMENT     ; set segment registers
+    mov ds, ax
+    mov es, ax
+
+    jmp KERNEL_LOAD_SEGMENT:KERNEL_LOAD_OFFSET
+
+    ; we should never reach here... I hope
+    jmp wait_key_and_reboot
+
+    cli                             ; disable interrupts, this way the CPU cannot leave "halt" state
+    hlt
+
+;
+; Error handlers
+;
+
+floppy_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+
+kernel_not_found_error:
+    mov si, msg_kernel_not_found
+    call puts
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h             ; wait for keypress
+    jmp 0FFFFh:0        ; jmp to beginning of BIOS/reboot
+
+.halt:
+    cli                 ; disable interrupts, this way the CPU cannot leave "halt" state
+    hlt
 
 ;
 ; Prints a string to the screen.
@@ -47,6 +232,7 @@ puts:
     ; save registers we will modify
     push si
     push ax
+    push bx
 
 .loop:
     lodsb               ; loads next character in al
@@ -60,54 +246,10 @@ puts:
     jmp .loop
 
 .done:
+    pop bx
     pop ax
     pop si
     ret
-
-
-main:
-    ; setup data segments
-    mov ax, 0           ; because we cannot write to ds/es directly
-    mov ds, ax
-    mov es, ax
-
-    ; setup stack
-    mov ss, ax
-    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
-
-    ; read something from floppy disk
-    ; BIOS should set DL to drive number
-    mov [ebr_drive_number], dl
-
-    mov ax, 1           ; LBA=1, second sector from disk
-    mov cl, 1           ; 1 sector to read
-    mov bx, 0x7E00      ; data should be after the bootloader
-    call disk_read
-
-    ; print message
-    mov si, msg_hello
-    call puts
-
-    cli                 ; disable interrupts, this way the CPU cannot leave "halt" state
-    hlt
-
-;
-; Error handlers
-;
-
-floppy_error:
-    mov si, msg_read_failed
-    call puts
-    jmp wait_key_and_reboot
-
-wait_key_and_reboot:
-    mov ah, 0
-    int 16h             ; wait for keypress
-    jmp 0FFFFh:0        ; jmp to beginning of BIOS/reboot
-
-.halt:
-    cli                 ; disable interrupts, this way the CPU cannot leave "halt" state
-    hlt
 
 ;
 ; Disk routines
@@ -213,8 +355,16 @@ disk_reset:
     ret
 
 
-msg_hello:              db 'Hello operating system!', ENDL, 0
+msg_loading:            db 'Loading...', ENDL, 0
 msg_read_failed:        db 'Could not read from disk!', ENDL, 0
+msg_kernel_not_found    db 'KERNEL.BIN not found!', ENDL, 0
+file_kernel_bin:        db 'KERNEL  BIN'
+kernel_cluster:         dw 0
+
+KERNEL_LOAD_SEGMENT     equ 0x2000
+KERNEL_LOAD_OFFSET      equ 0
 
 times 510-($-$$) db 0
 dw 0AA55h
+
+buffer:
